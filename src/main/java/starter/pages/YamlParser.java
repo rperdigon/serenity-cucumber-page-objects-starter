a7ca736b98007be7e49a2c03a06bf66bc4907820
ruben <rperdigon.b9@gmail.com>
2024-06-06 17:06:28 +0200

    added change for yamel parser

--- src/main/java/starter/pages/YamlParser.java
+++ src/main/java/starter/pages/YamlParser.java
@@ -1,6 +1,7 @@
 package starter.pages;
 
-import net.serenitybdd.core.Serenity;
+import net.thucydides.model.environment.SystemEnvironmentVariables;
+import net.thucydides.model.util.EnvironmentVariables;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.error.YAMLException;
 import org.openqa.selenium.By;
@@ -9,11 +10,22 @@ import org.openqa.selenium.WebElement;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class YamlParser {
+
+    private final String directoryPath;
+
+    public YamlParser() {
+        // Leer la configuración de Serenity
+        EnvironmentVariables environmentVariables = SystemEnvironmentVariables.createEnvironmentVariables();
+        directoryPath = environmentVariables.getProperty("serenity.yaml.directory.path");
+    }
 
     public List<Map<String, String>> getSelectors(String filePath) {
         List<Map<String, String>> selectors = new ArrayList<>();
@@ -40,6 +52,20 @@ public class YamlParser {
             e.printStackTrace();
         }
         return selectors;
+    }
+
+    public List<Map<String, String>> getAllSelectors() {
+        List<Map<String, String>> allSelectors = new ArrayList<>();
+        try {
+            Files.list(Paths.get(directoryPath))
+                    .filter(Files::isRegularFile)
+                    .filter(path -> path.toString().endsWith(".yaml"))
+                    .forEach(path -> allSelectors.addAll(getSelectors(path.toString())));
+        } catch (IOException e) {
+            System.out.println("Error al leer los archivos del directorio: " + directoryPath);
+            e.printStackTrace();
+        }
+        return allSelectors;
     }
 
     public WebElement findElement(WebDriver driver, Map<String, String> elementData) {
@@ -71,21 +97,5 @@ public class YamlParser {
                 return null;
         }
         return driver.findElement(by);
-    }
-
-    public static void main(String[] args) {
-        YamlParser parser = new YamlParser();
-        String filePath = "/Users/bcnc/IdeaProjects/serenity-cucumber-page-objects-starter/src/main/java/starter/pages/archivo.yaml";
-
-        List<Map<String, String>> selectors = parser.getSelectors(filePath);
-        for (Map<String, String> selector : selectors) {
-            WebElement element = parser.findElement(Serenity.getDriver(), selector);
-            if (element != null) {
-                System.out.println("Elemento " + selector.get("Name") + " encontrado: " + element.getText());
-                System.out.println(selector);
-            } else {
-                System.out.println("No se pudo encontrar el elemento " + selector.get("Name"));
-            }
-        }
     }
 }
